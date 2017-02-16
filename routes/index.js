c64e51fc6671e64806e0dfdad358e174581691a4
Mario Mol <mariohmol@gmail.com>
2017-02-16 20:28:10 -0200

    people view test

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var person = require('../controllers/people.ctrl')
+var people = require('../controllers/people.ctrl')
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -12,15 +12,15 @@ router.get('/person/new', function(req, res) {
 });
 
 router.get('/people', function(req, res, next) {
-  res.render('people', { people: people.list });
+  res.render('people', { people: people.getAll(), test: "hello" });
 });
 
 //syntax below taken from article
 //hackhands.com/mongodb-crud-mvc-way-with-passport-authentication
 router.post('/person/create', function(req, res, next) {
-  console.log(req.body)
-  var newPerson = req.body
-  console.log(newPerson)
+  console.log(req.body);
+  var newPerson = req.body;
+  console.log(newPerson);
   person.create(newPerson);
   res.redirect('/');
 });
