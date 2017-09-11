570224755cf4c508ca2efa7030f906f7cea38d12
mpstaton <mpstaton@gmail.com>
2017-09-10 19:48:06 -0700

    the files I didn't submit on interactions

---
+++ controllers/interaction.ctrl.js
@@ -0,0 +1,59 @@
+var Interaction = require('../models/interaction.model');
+
+module.exports = {
+
+    getAll: function(req, res, next) {
+        Interaction.find(function(err, contacts) {
+          if (err) return res.status(400).json(err);
+    
+          res.status(200).json(interactions);
+        });
+      },
+    getAllByPerson: function(req, res, next) {
+        //need to figure out best way to query interactions through the Person component
+        //may need to push created interactions into an interactions array in the Person model
+        Interaction.find({includedPeople: req.params.id},function(err, interactions) {
+          if (err) return res.status(400).json(err);
+          res.status(200).json(interactions);
+        });
+      },
+    getAllByOrganization: function(req, res, next) {
+        //need to figure out best way to query interactions through the Organization component
+        //may need to push created interactions into an interactions array in the Organization model
+        Interaction.find({includedPeople: req.params.id},function(err, interactions) {
+          if (err) return res.status(400).json(err);
+          res.status(200).json(interactions);
+        });
+      },
+    getOne: function(req, res, next) {
+        Interaction.findOne({ _id: req.params.id })
+        .populate('interactions')
+        .exec(function(err, interaction) {
+          if (err) return res.status(400).json(err);
+          if (!interaction) return res.status(404).json();
+    
+          res.status(200).json(interaction);
+        });
+      },
+    createOne: function(req, res, next) {
+        Interaction.create(req.body, function(err, interaction) {
+          if (err) return res.status(400).json(err);
+          res.status(201).json(interaction);
+        });
+      },
+    updateOne: function(req, res, next) {
+        Interaction.findOneAndUpdate({ _id: req.params.id }, req.body, {new: true}, function(err, interaction) {
+          if (err) return res.status(400).json(err);
+          if (!interaction) return res.status(404).json();
+    
+          res.status(200).json(interaction);
+        });
+      },
+    deleteOne: function(req, res, next) {
+        Interaction.findOneAndRemove({ _id: req.params.id }, function(err) {
+          if (err) return res.status(400).json(err);
+    
+          res.status(204).json();
+        });
+      },  
+};
---
+++ routes/interaction.routes.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+var passport = require('passport');
+var interactionCtrl = require('../controllers/interaction.ctrl');
+
+router.get('/getAll', interactionCtrl.getAll);
+router.get('/find', interactionCtrl.find);
+router.post('/new', interactionCtrl.createOne);
+router.get('/:interaction',interactionCtrl.getOne);
+router.put('/:interaction',interactionCtrl.updateOne);
+router.delete('/:interaction',interactionCtrl.deleteOne);
+
+
+module.exports = router;
